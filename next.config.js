e4fbba8556b810568e99a857b183ffbd936cece6
majickdave <majickdave@gmail.com>
2025-03-03 17:42:10 -0800

    fixed dev server

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
+    // Add experimental configuration for middleware
+    experimental: {}
 }
 
 module.exports = nextConfig
